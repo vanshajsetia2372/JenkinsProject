fc7e5a99a8897069e04101db4391987f7fa45fec
Vanshaj Setia <vanshaj.setia@nagarro.com>
2020-07-18 01:42:21 -0400

    New Commit

---
+++ GitPractice.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class GitPractice
+{
+	public static void main(String args[])
+	{
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("The number in the program is: "+i);
+		}
+	}
+}
